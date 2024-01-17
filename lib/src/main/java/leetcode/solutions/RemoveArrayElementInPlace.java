dc11b0a9c6fa52620699e16fc463018cda56f9d6
Ilya Palopezhentsev <iliks@web.de>
2024-01-17 20:18:34 +0100

    optimize a bit

--- lib/src/main/java/leetcode/solutions/RemoveArrayElementInPlace.java
+++ lib/src/main/java/leetcode/solutions/RemoveArrayElementInPlace.java
@@ -5,16 +5,14 @@ package leetcode.solutions;
  */
 public class RemoveArrayElementInPlace {
     public static int removeElement(int[] nums, int val) {
-        int countNotEqualToVal = 0;
         int idxSlow = 0;
         for (int idxFast = 0; idxFast < nums.length; idxFast++) {
             int cur = nums[idxFast];
             if (cur != val) {
-                countNotEqualToVal++;
                 nums[idxSlow++] = cur;
             }
         }
-        return countNotEqualToVal;
+        return idxSlow;
     }
 
 }
--- lib/src/main/java/leetcode/solutions/RemoveDuplicatesFromSortedArray.java
+++ lib/src/main/java/leetcode/solutions/RemoveDuplicatesFromSortedArray.java
@@ -7,15 +7,13 @@ public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
         int idxSlow = 0;
         int prev = nums[0];
-        int countUnique = 1;
         for (int idxFast = 1; idxFast < nums.length; idxFast++) {
             int cur = nums[idxFast];
             if (cur != prev) {
                 nums[++idxSlow] = cur;
-                countUnique++;
             }
             prev = cur;
         }
-        return countUnique;
+        return idxSlow + 1;
     }
 }
